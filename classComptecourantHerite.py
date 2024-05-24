37b34d479b65174c57394da6bacf290e72059780
albersonndaliko <albertwork40@gmail.com>
2024-05-24 13:11:03 +0200

    classComptecourantHerite qui herite la class compte

---
+++ classComptecourantHerite.py
@@ -0,0 +1,13 @@
+import MonMain 
+# Classe CompteCourant hérite de Compte et implémente la méthode retirer
+class CompteCourant(Compte):
+    def __init__(self, titulaire, solde=0):
+        super().__init__(titulaire, solde)  
+# Appel du constructeur de la classe parent
+
+ # Surcharge de la méthode retirer pour le CompteCourant
+    def retirer(self, montant):
+        if montant <= self._solde:
+            self._solde =self._solde - montant
+            return self._solde
+        return "impossible de retirer car le montant est inferieur au solde" 
